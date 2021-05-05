f29d83f7c128b12d45e073b03814c85ffc8f8102
fange <aaa@123.com>
2021-05-05 18:05:05 +0800

    new

--- myengin/main.cpp
+++ myengin/main.cpp
@@ -3,21 +3,34 @@
 #include<iostream>
 #include<GL/glew.h>
 #include<GLFW/glfw3.h>
+#include"Shader.h"
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+float vertics[] = {
+	0.5f, 0.5f, 0.0f,   // 右上角
+	0.5f, -0.5f, 0.0f,  // 右下角
+	-0.5f, -0.5f, 0.0f, // 左下角
+	-0.5f, 0.5f, 0.0f   // 左上角
+};
 
+unsigned int indices[] = { // 注意索引从0开始! 
+	0, 1, 3, // 第一个三角形
+	1, 2, 3  // 第二个三角形
+};
 
 
 
 int main() {
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//------------------------------------------
 	//create window
 	GLFWwindow* window = glfwCreateWindow(800, 600, "openglwindow", NULL, NULL);
 
@@ -30,18 +43,40 @@ int main() {
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-
+	//------------------------------------------
 	//init glew
-
 	glewExperimental = true;
 	if (glewInit() != GLEW_OK) {
 		std::cout << "init glew fail" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-
-
 	glViewport(0, 0, 800, 600);
+
+	Shader* myShader = new Shader("vertexSourceCode.txt", "fragmentSourceCode.txt");
+
+	//------------------------------------------
+
+	unsigned int VAO;
+	unsigned int VBO;
+	unsigned int EBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO); //1 是 缓冲ID
+	glGenBuffers(1, &EBO);
+
+	
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //GL_ARRAY_BUFFER是唯一的
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertics), vertics, GL_STATIC_DRAW);//VBO内容：全是数据顶点
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); //VAO内容：告诉你几个为一组
+	glEnableVertexAttribArray(0);
+	
+
+
 
 	while (!glfwWindowShouldClose(window)) {
 
@@ -51,9 +86,11 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		myShader->use();
 
-
-
+		glBindVertexArray(VAO);// seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glBindVertexArray(0);
 
 
 
@@ -65,7 +102,7 @@ int main() {
 
 
 
-
+	//------------------------------------------
 	glfwTerminate();
 	return 0;
 }
